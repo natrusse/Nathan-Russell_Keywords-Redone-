db87457bfc042465db387e26ba353db1c3b72c00
KNOWN-UNIVERSE\natrusse <natrusse@uat.edu>
2018-02-11 09:12:11 -0700

    Base code for 10 keywords project.

--- Nathan Russell_Keywords/Nathan Russell_Keywords.cpp
+++ Nathan Russell_Keywords/Nathan Russell_Keywords.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace::std;
 
 
 int main()
 {
+	enum fields {WORD, HINT, NUM_FIELDS};
+	const int NUM_WORDS = 5;
+	const string WORDS[NUM_WORDS][NUM_FIELDS] =
+	{
+		{"Wall", "Do you feel you're head banging against something?"},
+		{"glasses", "These might help you see the answer."},
+		{"labored", "Going slowly, is it?"},
+		{"persistent", "Keep at it."},
+		{"jumble", "It's what the game is all about."}
+	};
+
+	srand(static_cast<unsigned int>(time(0)));
+	int choice = (rand() % NUM_WORDS);
+	string theWord = WORDS[choice][WORD];
+	string theHint = WORDS[choice][HINT];
+
+	string jumble = theWord;
+	int length = jumble.size();
+	for (int i = 0; i < length; ++i)
+	{
+		int index1 = (rand() % length);
+		int index2 = (rand() % length);
+		char temp = jumble[index1];
+		jumble[index1] = jumble[index2];
+		jumble[index2] = temp;
+
+	}
+
     return 0;
 }
 
